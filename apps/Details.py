2279509ef6760c16d3811a25cd9b7d9d6565cc8c
Samya Goswami <samyagoswami04@gmail.com>
2023-02-26 19:51:22 +0530

    Update Details.py

--- apps/Details.py
+++ apps/Details.py
@@ -1,4 +1,7 @@
 import streamlit as st
 
 def app():
-    pass
+    st.write("Tuhin Mondal (CSE AIML 2C, UEMK) -> MACHINE LEARNING and backend")
+    st.write("Samya Goswami (CSE AIML 2B, UEMK) -> MACHINE LEARNING")
+    st.write("Ankan Hazra (CSE 2A, UEMK) -> FRONTEND")
+    st.write("Rounak Dey (CSE IOT, UEMK) -> UI/UX")
